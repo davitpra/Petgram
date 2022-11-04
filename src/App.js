cf1b0f474a08fca8b7920623b420bb0f4521a2fe
David <j.david.prado1716@gmail.com>
2022-11-04 11:48:59 -0500

    creando el primer componente: Category

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Category } from './components/Category'
+
+function App () {
+  return (
+    <Category />
+  )
+}
+
+export default App
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import App from './App'
 
 const root = ReactDOM.createRoot(document.getElementById('app'))
-root.render(<h1>holis</h1>)
+root.render(<App />)
