ebb1660ab5a192b4baeb320bea0f2b24e70e912a
David <j.david.prado1716@gmail.com>
2022-11-05 10:05:33 -0500

    crando el Logo SVG a componente de React

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react'
 import { ListOfCategories } from './components/ListOfCategories'
 import { GlobalStyle } from './GlobalStyles'
 import { ListOfPhotoCards } from './components/ListOfPhotoCards'
+import { Logo } from './components/Logo'
 
 function App () {
   return (
     <>
       <GlobalStyle />
+      <Logo />
       <ListOfCategories />
       <ListOfPhotoCards />
     </>
--- src/components/PhotoCard/index.js
+++ src/components/PhotoCard/index.js
@@ -3,6 +3,7 @@ import { ImgWrapper, Img, Button } from './styles'
 import { MdFavoriteBorder } from 'react-icons/md'
 
 const DEFAULT_IMAGE = 'https://res.cloudinary.com/midudev/image/upload/w_300/q_80/v1560262103/dogs.png'
+
 export function PhotoCard ({ id, likes = 0, src = DEFAULT_IMAGE }) {
   return (
     <article>
