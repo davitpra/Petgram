8e81688cce7d076b6da66ade2ffcfbe07764f271
David <j.david.prado1716@gmail.com>
2022-11-07 21:28:57 -0500

    separando la logica LS y nearScreen en custume Hooks

--- src/components/PhotoCard/index.js
+++ src/components/PhotoCard/index.js
@@ -1,49 +1,27 @@
-import React, { useRef, useEffect, useState } from 'react'
+import React from 'react'
 import { Article, ImgWrapper, Img, Button } from './styles'
 import { MdFavoriteBorder, MdFavorite } from 'react-icons/md'
+import { useNearScreen } from '../hooks/useNearScreen'
+import { useLocalStorage } from '../hooks/useLocalStorage'
 
 const DEFAULT_IMAGE = 'https://res.cloudinary.com/midudev/image/upload/w_300/q_80/v1560262103/dogs.png'
 
 export function PhotoCard ({ id, likes = 0, src = DEFAULT_IMAGE }) {
+  const [show, element] = useNearScreen()
+
   const key = `like-${id}`
-  const [show, setShow] = useState(false)
-  const [like, setlike] = useState(() => {
-    try {
-      const value = window.localStorage.getItem(key)
-      return JSON.parse(value)
-    } catch (e) {
-      return false
-    }
-  })
-
-  const element = useRef(null)
-
-  useEffect(() => {
-    Promise.resolve(
-      typeof window.IntersectionObserver !== 'undefined'
-        ? window.IntersectionObserver
-        : import('intersection-observer')
-    ).then(() => {
-      const observer = new window.IntersectionObserver(function (entries) {
-        if (entries[0].isIntersecting) {
-          setShow(true)
-          observer.disconnect()
-        }
-      })
-      observer.observe(element.current)
-    })
-  }, [element])
+  const [liked, setLiked] = useLocalStorage(key, false)
 
   const togglelike = (value) => {
     try {
       window.localStorage.setItem(key, value)
-      setlike(value)
+      setLiked(value)
     } catch (e) {
       return false
     }
   }
 
-  const Icon = like ? MdFavorite : MdFavoriteBorder
+  const Icon = liked ? MdFavorite : MdFavoriteBorder
 
   return (
     <Article ref={element}>
@@ -54,7 +32,7 @@ export function PhotoCard ({ id, likes = 0, src = DEFAULT_IMAGE }) {
               <Img src={src} />
             </ImgWrapper>
           </a>
-          <Button onClick={() => togglelike(!like)}>
+          <Button onClick={() => togglelike(!liked)}>
             <Icon size='32px' />{likes} likes!
           </Button>
         </>}
