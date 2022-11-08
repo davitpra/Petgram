e768deaa8d6736437389f2d2a638e14375275ef6
David <j.david.prado1716@gmail.com>
2022-11-08 11:17:48 -0500

    Inicializando React Apollo Client

--- src/components/ListOfPhotoCards/index.js
+++ src/components/ListOfPhotoCards/index.js
@@ -1,10 +1,38 @@
 import React from 'react'
+import { useQuery, gql } from '@apollo/client'
 import { PhotoCard } from '../PhotoCard'
 
+// gql nos permite transformar la peticion la query en string.
+// query es la peticion para obtener la información, puedes verla dentro del playground en el endpoint de la peticion.
+// hacemos la peticion getphotos y obtenermos photos con todos esas key.
+const getPhotos = gql`
+  query getPhotos {
+    photos {
+      id
+      categoryId
+      src
+      likes
+      userId
+      liked
+    }
+  }
+`
+
 export function ListOfPhotoCards () {
+  const { loading, error, data } = useQuery(getPhotos)
+
+  if (error) {
+    return <h2>Internal Server Error</h2>
+  }
+  if (loading) {
+    return <h2>Loading...</h2>
+  }
+
   return (
     <ul>
-      {[7, 8, 9, 10, 11, 12].map(id => <PhotoCard key={id} id={id} />)}
+      {data.photos.map((photo) => (
+        <PhotoCard key={photo.id} {...photo} />
+      ))}
     </ul>
   )
-}
+};
--- src/index.js
+++ src/index.js
@@ -1,6 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+// importamos las dependencias.
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client'
 import App from './App'
 
+// inicalizamos el cliente de esta manera:
+const client = new ApolloClient({
+  // es URI NO URL.
+  uri: 'https://petgram-dbyte-davitpra.vercel.app/graphql',
+  cache: new InMemoryCache()
+})
+
 const root = ReactDOM.createRoot(document.getElementById('app'))
-root.render(<App />)
+root.render(
+  // Conectamos el cliente con la APP
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>
+)
