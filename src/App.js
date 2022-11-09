207ddbf8ae100c1ea19f3ac085ede87b7efbe95c
David <j.david.prado1716@gmail.com>
2022-11-09 18:41:38 -0500

    añadiendo navbar"

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { GlobalStyle } from './styles/GlobalStyles'
 import { Logo } from './components/Logo'
 import { Home } from './pages/Home'
 import { Detail } from './pages/Detail'
+import { NavBar } from './components/NavBar'
 import { Route, Routes, BrowserRouter } from 'react-router-dom'
 
 function App () {
@@ -16,6 +17,7 @@ function App () {
           <Route path='pet/:id' element={<Home />} />
           <Route path='/detail/:detailId' element={<Detail />} />
         </Routes>
+        <NavBar />
       </BrowserRouter>
     </>
   )
