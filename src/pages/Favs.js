47a0696a67abec661abefee19f5d50c15cd54ac3
David <j.david.prado1716@gmail.com>
2022-11-15 11:45:56 -0500

    React.lazy() y componente Suspense

--- src/pages/Favs.js
+++ src/pages/Favs.js
@@ -2,15 +2,14 @@ import React from 'react'
 import { Meta } from '../components/Meta/index.js'
 import { GetFavorites } from '../container/GetFavorites.js'
 
-export function Favs () {
-  return (
-    <>
-      <Meta
-        metaTitle='Favoritos'
-        title='Tus favoritos'
-        subtitle='Separa las fotos que te gustan con un like'
-      />
-      <GetFavorites />
-    </>
-  )
-}
+export default () => (
+
+  <>
+    <Meta
+      metaTitle='Favoritos'
+      title='Tus favoritos'
+      subtitle='Separa las fotos que te gustan con un like'
+    />
+    <GetFavorites />
+  </>
+)
--- src/router/LayOut.js
+++ src/router/LayOut.js
@@ -1,11 +1,13 @@
-import React, { useContext } from 'react'
+import React, { Suspense, useContext } from 'react'
 import { Home } from '../pages/Home'
 import { Detail } from '../pages/Detail'
 import { NotRegisteredUser } from '../pages/NotRegisteredUser'
-import { Favs } from '../pages/Favs'
+// import { Favs } from '../pages/Favs'
 import { User } from '../pages/User'
 import { Route, Routes } from 'react-router-dom'
 import { Context } from '../context/Context'
+
+const Favs = React.lazy(() => import('../pages/Favs'))
 
 function UserLog ({ children }) {
   const { isAuth } = useContext(Context)
@@ -14,25 +16,27 @@ function UserLog ({ children }) {
 
 export function LayOut () {
   return (
-    <Routes>
-      <Route path='/' element={<Home />} />
-      <Route path='pet/:id' element={<Home />} />
-      <Route path='/detail/:detailId' element={<Detail />} />
-      <Route
-        path='/favs' element={
-          <UserLog>
-            <Favs />
-          </UserLog>
+    <Suspense fallback={<div />}>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='pet/:id' element={<Home />} />
+        <Route path='/detail/:detailId' element={<Detail />} />
+        <Route
+          path='/favs' element={
+            <UserLog>
+              <Favs />
+            </UserLog>
       }
-      />
-      <Route
-        path='/user' element={
-          <UserLog>
-            <User />
-          </UserLog>
+        />
+        <Route
+          path='/user' element={
+            <UserLog>
+              <User />
+            </UserLog>
       }
-      />
-      <Route path='*' element={<Home />} />
-    </Routes>
+        />
+        <Route path='*' element={<Home />} />
+      </Routes>
+    </Suspense>
   )
 }
